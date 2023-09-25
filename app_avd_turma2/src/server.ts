9af76209a60329240aae83e52e49247d10ea695d
Paulo Vitor Freitas da Costa <107567673+pegadalg@users.noreply.github.com>
2023-09-25 19:34:49 -0300

    Add files via upload

---
+++ app_avd_turma2/src/server.ts
@@ -0,0 +1,15 @@
+import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+const app = fastify();
+const prisma = new PrismaClient();
+
+app.listen({
+    port:3333,
+}).then(() => {
+    console.log('HTTP server running on http://localhost:3333')
+})
+
+app.get('/events',async () => {
+    const events = await prisma.events.findMany();
+    return events
+})
